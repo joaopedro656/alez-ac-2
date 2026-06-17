e60615969cb4cdb0f03ba3211464fbe0df7f5791
joaopedro656 <joaopedacademico@gmail.com>
2026-06-17 09:51:43 -0300

    dsa

--- src/app/ator-detalhe/ator-detalhe.page.ts
+++ src/app/ator-detalhe/ator-detalhe.page.ts
@@ -1,24 +1,26 @@
-import { Router } from '@angular/router'
+import { Router } from '@angular/router';
+import { IAtor } from './../model/IAtor';
 import { Component, OnInit } from '@angular/core';
-import { IAtor } from './../model/IAtor'
 import { ActivatedRoute } from '@angular/router';
 
+
 @Component({
-  selector: 'app-ator-detalhe',
-  templateUrl: './ator-detalhe.page.html',
-  styleUrls: ['./ator-detalhe.page.scss'],
-  standalone: false
+    selector: 'app-ator-detalhe',
+    templateUrl: './ator-detalhe.page.html',
+    styleUrls: ['./ator-detalhe.page.scss'],
+      standalone: false 
 })
 export class AtorDetalhePage implements OnInit {
-ator: any;
-  constructor(private route: ActivatedRoute, private router: Router) { }
-  ngOnInit() {
-    this.route.queryParams.subscribe(params => {
-      const getNav = this.router.getCurrentNavigation();
-      if (getNav?.extras.state){
-        this.ator = getNav.extras.state['paramAtor']
-      }
-    })
-  }
+  ator: any;
 
+    constructor(private route: ActivatedRoute, private router: Router) { }
+
+    ngOnInit() {
+      this.route.queryParams.subscribe(params => {
+        const getNav = this.router.getCurrentNavigation();
+        if (getNav?.extras.state) {
+          this.ator = getNav.extras.state['paramAtor']; 
+        }
+      })
+    }
 }
